38418b4af3e376543c291101786734a91c519b19
vikkkaa22 <kusya2203@gmail.com>
2025-09-19 12:20:30 +0500

    create new card

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const card = document.querySelector('.card'); //выбираем все элементы с классом card на странице
+document.querySelector(".container").addEventListener('click', ()=>{console.log(0)
+  card.classList.toggle('card-show')
+});
+
